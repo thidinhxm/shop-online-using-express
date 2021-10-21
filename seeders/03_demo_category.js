3a709b669868307980b91a9a30544fe2272c6f36
thidinhxm <thidinhxm@gmail.com>
2021-10-21 10:09:40 +0700

    update seeder category

--- seeders/03_demo_category.js
+++ seeders/03_demo_category.js
@@ -3,34 +3,13 @@
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         const data = [
-            {
-                name: 'Men',
-                imagepath:'/img/home/hero-slide1.png'
-            },
-            {
-                name: 'Women',
-                imagepath:'/img/home/hero-slide2.png'
-            },
-            {
-                name: 'Accessories',
-                imagepath:'/img/home/hero-slide3.png'
-            },
-            {
-                name: 'Footwear',
-                imagepath:'/img/home/hero-slide1.png'
-            },
-            {
-                name: 'Bay item',
-                imagepath:'/img/home/hero-slide2.png'
-            },
-            {
-                name: 'Electronics',
-                imagepath:'/img/home/hero-slide3.png'
-            },
-            {
-                name: 'Food',
-                imagepath:'/img/home/hero-slide2.png'
-            },
+            { name: 'Men', imagepath: '/img/home/hero-slide1.png', summary: "Men item" },
+            { name: 'Women', imagepath: '/img/home/hero-slide3.png', summary: "Women item" },
+            { name: 'Accessories', imagepath: '/img/home/hero-slide2.png', summary: "Accessories item" },
+            { name: 'Footwear', imagepath: '/img/home/hero-slide1.png', summary: "Footwear item" },
+            { name: 'Bay item', imagepath: '/img/home/hero-slide2.png', summary: "Bay item" },
+            { name: 'Electronics', imagepath: '/img/home/hero-slide3.png', summary: "Electronics item" },
+            { name: 'Food', imagepath: '/img/home/hero-slide2.png', summary: "Food item" },
         ]
 
         data.map(item => {
